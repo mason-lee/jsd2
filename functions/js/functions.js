395ee93a9f2a032454af8c4247085dd462d9e177
mason-lee <mason.lee@adroll.com>
2016-07-08 20:19:24 -0700

    functions are working..

---
+++ functions/js/functions.js
@@ -0,0 +1,24 @@
+function subtract(x, y) {
+    var result = x - y;
+    return result;
+}
+
+function add(x, y) {
+    var result = x + y;
+    return result;
+}
+
+function multiply(x, y) {
+    var result = x * y;
+    return result;
+}
+
+function divide(x, y) {
+    var result = x / y;
+    return result;
+}
+
+function remainder(x, y) {
+    var result = x % y;
+    return result;
+}
