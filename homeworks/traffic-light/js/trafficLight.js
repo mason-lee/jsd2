731aa882f04745eb6a694276fb7f2b2eb4f27a71
mason-lee <mason.lee@adroll.com>
2016-08-07 19:19:51 -0700

    After class

--- homeworks/traffic-light/js/trafficLight.js
+++ homeworks/traffic-light/js/trafficLight.js
@@ -1,7 +1,9 @@
 // Setup
 // ----------------------------------------------
 var cautionFlashOn = false;
-var cautionFlash;
+var cautionFlash = 0;
+var goTrafficLightOn = false;
+var goTrafficLight = 0;
 
 // Structure
 // ----------------------------------------------
@@ -10,6 +12,7 @@ var stopBtn = document.querySelector(".stop-button");
 var slowBtn = document.querySelector(".slow-button");
 var goBtn = document.querySelector(".go-button");
 var cautionBtn = document.querySelector(".caution-button");
+var runBtn = document.querySelector(".run-button");
 
 // Traffic Lights
 var trafficLight = document.querySelector("#traffic-light");
@@ -23,33 +26,58 @@ stopBtn.addEventListener('click', stopFlash);
 slowBtn.addEventListener('click', slowFlash);
 goBtn.addEventListener('click', goFlash);
 cautionBtn.addEventListener('click', cautionFlash);
+runBtn.addEventListener('click', run);
 
 // Event handlers
 // ----------------------------------------------
 function stopFlash() {
     trafficLight.classList.remove("go", "slow");
+
     if (cautionFlashOn === true) {
         clearInterval(cautionFlash);
         cautionFlashOn = false;
     }
+
+    if (goTrafficLightOn == true) {
+        clearInterval(goTrafficLight);
+        goTrafficLight = 0;
+        goTrafficLightOn = false;
+    }
+
     trafficLight.classList.add('stop');
 }
 
 function slowFlash() {
     trafficLight.classList.remove("go", "stop");
+
     if (cautionFlashOn === true) {
         clearInterval(cautionFlash);
         cautionFlashOn = false;
     }
+
+    if (goTrafficLightOn == true) {
+        clearInterval(goTrafficLight);
+        goTrafficLight = 0;
+        goTrafficLightOn = false;
+    }
+
     trafficLight.classList.add('slow');
 }
 
 function goFlash() {
     trafficLight.classList.remove("stop", "slow");
+
     if (cautionFlashOn === true) {
         clearInterval(cautionFlash);
         cautionFlashOn = false;
     }
+
+    if (goTrafficLightOn == true) {
+        clearInterval(goTrafficLight);
+        goTrafficLight = 0;
+        goTrafficLightOn = false;
+    }
+
     trafficLight.classList.add('go');
 }
 
@@ -58,7 +86,15 @@ function goFlash() {
 // Bonus
 //-------------------------------------------------------
 function cautionFlash() {
+    console.log("are we calling it?");
     trafficLight.classList.remove("go", "stop", "slow");
+
+    if (goTrafficLightOn == true) {
+        clearInterval(goTrafficLight);
+        goTrafficLight = 0;
+        goTrafficLightOn = false;
+        console.log("running");
+    }
 
     if (cautionFlashOn === false) {
         cautionFlash = setInterval(function() {
@@ -69,4 +105,57 @@ function cautionFlash() {
     else {
         console.log("It's already on.");
     }
+}
+
+// -----------------------------------------------------
+// Super Bonus
+//-------------------------------------------------------
+var lightTiming = 1000;
+
+// Define traffic light behavior
+function goTraffic() {
+    // Turn on red light
+    trafficLight.classList.add('stop');
+    // Turn off red light
+    setTimeout(function() {
+        trafficLight.classList.remove('stop');
+    }, lightTiming);
+    // Turn on caution light
+    setTimeout(function() {
+        trafficLight.classList.add('slow');
+    },lightTiming);
+    // Turn off caution light
+    setTimeout(function() {
+        trafficLight.classList.remove('slow');
+    },lightTiming * 2);
+    // Turn on go light
+    setTimeout(function() {
+        trafficLight.classList.add('go');
+    },lightTiming * 2);
+    // Turn off go light
+    setTimeout(function() {
+        trafficLight.classList.remove('go');
+    },lightTiming * 3);
+}
+
+function run() {
+    // Clear out all other Lights
+    trafficLight.classList.remove("go", "stop", "slow");
+
+    // Stop the caution flash
+    if (cautionFlashOn === true) {
+        clearInterval(cautionFlash);
+        cautionFlashOn = false;
+    }
+
+    if (goTrafficLightOn == false) {
+        // Call first
+        goTraffic();
+
+        // And repeat
+        goTrafficLight = setInterval(goTraffic, lightTiming * 3);
+
+        goTrafficLightOn = true;
+    }
+
 }
---
+++ practice/foodie/js/foodie.js
@@ -0,0 +1,44 @@
+// Structure
+// ------------------------------------
+var results = document.querySelector(".results");
+var form = document.querySelector("form");
+var zip = document.querySelector(".zip");
+
+
+// Events
+// ------------------------------------
+form.addEventListener('submit', getRestaurants);
+
+// Event Handler
+// ------------------------------------
+function getRestaurants(event) {
+    event.preventDefault();
+    var searchKeyword = zip.value;
+    var url = "https://opentable.herokuapp.com/api/restaurants?page=2&zip=" + searchKeyword;
+    $.getJSON(url, updateRestaurants);
+}
+
+// Update page
+// ------------------------------------
+function updateRestaurants(json) {
+    results.innerHTML = "";
+    json.restaurants.forEach(createRestaurant);
+    zip.value = "";
+
+}
+
+function createRestaurant(restaurant) {
+    var list = document.createElement("li");
+    var resName = document.createElement("h2");
+    var resAddress = document.createElement("p");
+    var resImage = document.createElement("img");
+
+    resName.innerHTML = restaurant.name;
+    resAddress.innerHTML = restaurant.address;
+    resImage.setAttribute("src", restaurant.image_url);
+
+    list.appendChild(resImage);
+    list.appendChild(resName);
+    list.appendChild(resAddress);
+    results.appendChild(list);
+}
